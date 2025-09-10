990b2128526bb4dc4e07cb15b3b997b2440c098f
DvoychenkovA <DvoychenkovA@terralink-global.com>
2025-09-10 17:32:12 +0300

    - checkstyle fix;

--- server/src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
+++ server/src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
@@ -11,8 +11,6 @@ import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ItemRequestMapper {
